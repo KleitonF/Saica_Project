1e1de9a9c4497eb1f2ea8f85a17a12ccf747d63f
Kleiton Fragoso <kleitonsilvafragoso@gmail.com>
2022-04-16 14:15:19 -0300

    correção da função apresentação

--- chaves_funcoes_memorias.py
+++ chaves_funcoes_memorias.py
@@ -1,15 +1,13 @@
 import memoria
 
 def ChaveApresentacaoMemorias():
-     chave_respostas = ['Pode me chamar de SAICA!\nQual é o seu nome?','Meu nome é SAICA.\nQual é o seu nome?','Eu me chamo SAICA.\nQual é o seu nome?',
-                       'Eu sou SAICA.\nQual é o seu nome?','Eu sou o Sistema Auxiliar Interativo Computacional Artificial.\nMas pode me chamar apenas de SAICA.\nQual é o seu nome?']
-     historico_respostas = memoria.LembrarRespostas()
-     historico_chave = [str(historico_respostas[-1])] + chave_respostas
-     qtd_resp = len( set( [ item for item in historico_chave if historico_chave.count( item ) > 1] ) )
-     return qtd_resp
+    historico_funcoes = memoria.LembrarFuncoes()
+    historico_chave = [str(historico_funcoes[-1])] + ["funcoes.apresentacao()"]
+    qtd_resp = len( set( [ item for item in historico_chave if historico_chave.count( item ) > 1] ) )
+    return qtd_resp
 
 def ChaveAgendamentoMemorias():
     historico_funcoes = memoria.LembrarFuncoes()
-    historico_chave = [str(historico_funcoes[-1])]+["funcoes.agendamento()"]
+    historico_chave = [str(historico_funcoes[-1])] + ["funcoes.agendamento()"]
     qtd_resp = len( set( [ item for item in historico_chave if historico_chave.count( item ) > 1] ) )
     return qtd_resp
